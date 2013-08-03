c96a2d238a2c318be211db56ce8356eadaaf2f61
Borislav Kosharov <boby_dsm@abv.bg>
2013-08-03 20:15:49 +0300

    Added spine.all
    This module imports all others publicly

---
+++ src/spine/all.d
@@ -0,0 +1,11 @@
+module spine.all;
+
+public {
+    import spine.animation;
+    import spine.atlas;
+    import spine.attachment;
+    import spine.bone;
+    import spine.skeleton;
+    import spine.skin;
+    import spine.slot;
+}
