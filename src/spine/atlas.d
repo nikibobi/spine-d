3882725d5cbcd06f4e4856b444f4e3aefc49db97
Borislav Kosharov <boby_dsm@abv.bg>
2013-08-03 20:03:28 +0300

    Implemented spine.atlas

---
+++ src/spine/atlas.d
@@ -0,0 +1,96 @@
+module spine.atlas;
+
+import std.stdio;
+
+export class Atlas {
+
+    this(File reader, string dir, TextureLoader textureLoader) {
+        load(reader, dir, textureLoader);
+    }
+
+    private void load(File reader, string imagesDir, TextureLoader textureLoader) {
+        //TODO: implement loading file
+    }
+
+    private static string readValue(File reader) {
+        //TODO: implement
+    }
+
+    private static int readTuple(File reader, string[] tuple) {
+        //TODO: implement
+    }
+
+    AtlasRegion findRegion(string name) {
+        foreach(region; _regions)
+            if(region.name == name)
+                return region;
+        return null;
+    }
+
+    void dispose() {
+        foreach(page; _pages)
+            textureLoader.unload(page.rendererObject);
+    }
+
+
+private:
+    AtlasPage[] _pages;
+    AtlasRegion[] _regions;
+    TextureLoader textureLoader;
+
+public:
+    enum Format {
+        Alpha,
+        Intensity,
+        LuminanceAlpha,
+        RGB565,
+        RGBA4444,
+        RGB888,
+        RGBA8888
+    }
+
+    enum TextureFilter {
+        Nearest,
+        Linear,
+        MipMap,
+        MipMapNearestNearest,
+        MipMapLinearNearest,
+        MipMapNearestLinear,
+        MipMapLinearLinear
+    }
+
+    enum TextureWrap {
+        MirroredRepeat,
+        ClampToEdge,
+        Repeat
+    }
+
+    class AtlasPage {
+        string name;
+        Format format;
+        TextureFilter minFilter;
+        TextureFilter magFilter;
+        TextureWrap uWrap;
+        TextureWrap vWrap;
+        Object rendererObject;
+        int width, height;
+    }
+
+    class AtlasRegion {
+        AtlasPage page;
+        string name;
+        int x, y, width, height;
+        float u, v, u2, v2;
+        float offsetX, offsetY;
+        int originalWidth, originalHeight;
+        int index;
+        bool rotate;
+        int[] splits;
+        int[] pads;
+    }
+
+    interface TextureLoader {
+        void load(AtlasPage page, string path);
+        void unload(Object texture);
+    }
+}
