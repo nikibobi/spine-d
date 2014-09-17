e298b072beaec1453a9e0d34cd44f3d406f53266
Borislav Kosharov <bosakmaw@gmail.com>
2014-09-17 21:41:46 +0300

    Added BoundingBox attachment

--- src/spine/attachment/all.d
+++ src/spine/attachment/all.d
@@ -3,6 +3,7 @@ module spine.attachment.all;
 public {
     import spine.attachment.atlasloader;
     import spine.attachment.attachment;
+    import spine.attachment.boundingbox;
     import spine.attachment.loader;
     import spine.attachment.region;
     import spine.attachment.type;
---
+++ src/spine/attachment/boundingbox.d
@@ -0,0 +1,39 @@
+module spine.attachment.boundingbox;
+
+import spine.attachment.attachment;
+import spine.bone.all;
+
+export class BoundingBoxAttachment : Attachment {
+    
+    this(string name) {
+        super(name);
+    }
+
+    @property {
+        float[] vertices() {
+            return _vertices;
+        }
+        void vertices(float[] value) {
+            _vertices = value;
+        }
+    }
+
+    void computeWorldVertices(float x, float y, Bone bone, float[] worldVertices) {
+        x += bone.worldX;
+        y += bone.worldY;
+        float m00 = bone.m00;
+        float m01 = bone.m01;
+        float m10 = bone.m10;
+        float m11 = bone.m11;
+        float[] vertices = vertices;
+        for(int i = 0; i < vertices.length; i += 2) {
+            float px = vertices[i];
+            float py = vertices[i + 1];
+            worldVertices[i] = px * m00 + py * m01 + x;
+            worldVertices[i + 1] = px * m10 + py * m11 + y;
+        }
+    }
+
+private:
+    float[] _vertices;
+}
