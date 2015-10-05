485a07c930ba1574fa593a86f61b03d5ad65c6a8
Borislav Kosharov <bosakmaw@gmail.com>
2015-10-05 15:45:13 +0300

    fix flipx and flipy timelines missing ctors

--- src/spine/animation/timeline/flipx.d
+++ src/spine/animation/timeline/flipx.d
@@ -4,6 +4,10 @@ import spine.animation.timeline.flip;
 import spine.bone.bone;
 
 export class FlipXTimeline : FlipTimeline {
+
+    this(int frameCount) {
+        super(frameCount);
+    }
     
     override protected void setFlip(Bone bone, bool flip) {
         bone.flipX = flip;
--- src/spine/animation/timeline/flipy.d
+++ src/spine/animation/timeline/flipy.d
@@ -4,6 +4,10 @@ import spine.animation.timeline.flip;
 import spine.bone.bone;
 
 export class FlipYTimeline : FlipTimeline {
+
+    this(int frameCount) {
+        super(frameCount);
+    }
     
     override protected void setFlip(Bone bone, bool flip) {
         bone.flipY = flip;
