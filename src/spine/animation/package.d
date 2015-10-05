3ea59f47fda35084b401b10b449f83616e746750
Borislav Kosharov <bosakmaw@gmail.com>
2015-10-05 22:41:52 +0300

    fix missing public imports for timelines

--- src/spine/animation/package.d
+++ src/spine/animation/package.d
@@ -6,12 +6,14 @@ public {
     import spine.animation.state.state;
     import spine.animation.timeline.attachment;
     import spine.animation.timeline.color;
+    import spine.animation.timeline.curve;
     import spine.animation.timeline.draworder;
     import spine.animation.timeline.event;
     import spine.animation.timeline.ffd;
     import spine.animation.timeline.flip;
     import spine.animation.timeline.flipx;
     import spine.animation.timeline.flipy;
+    import spine.animation.timeline.ikconstraint;
     import spine.animation.timeline.rotate;
     import spine.animation.timeline.scale;
     import spine.animation.timeline.timeline;
